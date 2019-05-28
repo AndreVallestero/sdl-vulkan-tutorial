48610b3ab6e706f2aa4d96fafee9e46a0852593d
Andre Vallestero <andrevallestero@gmail.com>
2019-05-28 17:15:58 -0400

    Minimum working window

--- hello-triangle/src/main.cpp
+++ hello-triangle/src/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
 
-int main(void) {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+int main(int argc, char * argv[]) {
+    SDL_Window *window;
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("hello-triangle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 360, SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
+    SDL_GL_CreateContext(window);
+
+    bool running = true;
+    while(running) {
+        SDL_Event windowEvent;
+        while(SDL_PollEvent(&windowEvent))
+            if(windowEvent.type == SDL_QUIT) {
+                running = false;
+                break;
+            }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
