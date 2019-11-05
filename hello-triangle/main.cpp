ceedefca580d43212f4d6c491ad71a409ffb952a
Andre Vallestero <andrevallestero@gmail.com>
2019-11-05 13:06:35 -0500

    Added resources

--- hello-triangle/main.cpp
+++ hello-triangle/main.cpp
@@ -1,94 +1,111 @@
 #include <iostream>
+#include <vector>
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wpragma-pack"
 #include <SDL/SDL.h>
 #include <SDL/SDL_vulkan.h>
 #pragma clang diagnostic pop
+#include <vulkan/vulkan.hpp>
 
 #ifdef _WIN32
     #pragma comment(linker, "/subsystem:windows")
     #define VK_USE_PLATFORM_WIN32_KHR
     #define PLATFORM_SURFACE_EXTENSION_NAME VK_KHR_WIN32_SURFACE_EXTENSION_NAME
-#else
-    #define PLATFORM_SURFACE_EXTENSION_NAME VK_KHR_XLIB_SURFACE_EXTENSION_NAME
 #endif
-
-#include <vulkan/vulkan.hpp>
 
 #define APP_NAME "hello-triangle"
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#ifndef UINT32_MAX
+    #define UINT32_MAX 0xffffffff
+#endif
 
-VkBool32 verify_instance_layers(const char** instanceLayerNames) {
-    uint32_t instanceLayerCount = 0;
-    vkEnumerateInstanceLayerProperties(&instanceLayerCount, NULL);
-    VkLayerProperties* instanceLayers = (VkLayerProperties*) malloc(sizeof (VkLayerProperties) * instanceLayerCount);
-    vkEnumerateInstanceLayerProperties(&instanceLayerCount, instanceLayers);
-
-    for (uint32_t i = 0; i < ARRAY_SIZE(instanceLayerNames); ++i)
-        for (uint32_t j = 0; j < instanceLayerCount; ++j)
-            if (!strcmp(instanceLayerNames[i], instanceLayers[j].layerName)) {
-                free(instanceLayers);
-                return 1;
-            }
-    free(instanceLayers);
-    return 0;
-}
-
-int main(int argc, char * argv[]) {
-    SDL_Window *window;
+int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow(APP_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 360, SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
-    SDL_GL_CreateContext(window);
+    SDL_Vulkan_LoadLibrary(nullptr);
+    SDL_Window* window = SDL_CreateWindow(APP_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 360, SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
 
-    VkApplicationInfo appInfo = {
-        VK_STRUCTURE_TYPE_APPLICATION_INFO, // sType
-        NULL,                               // pNext
-        APP_NAME,                           // pApplicationName
-        0,                                  // applicationVersion
-        APP_NAME,                           // pEngineName
-        0,                                  // engineVersion
-        VK_API_VERSION_1_0,                 // apiVersion
-    };
-
-    const char* instance_validation_layers[] = {
-        "VK_LAYER_LUNARG_standard_validation"
-    };
-    const char* instance_validation_layers_alt[] = {
-        "VK_LAYER_GOOGLE_threading",     "VK_LAYER_LUNARG_parameter_validation",
-        "VK_LAYER_LUNARG_device_limits", "VK_LAYER_LUNARG_object_tracker",
-        "VK_LAYER_LUNARG_image",         "VK_LAYER_LUNARG_core_validation",
-        "VK_LAYER_LUNARG_swapchain",     "VK_LAYER_GOOGLE_unique_objects"
-    };
-    VkBool32 useStdLayers = verify_instance_layers(instance_validation_layers);
-
-    uint32_t enabledExtensionCount = 0;
-    const char* extensionNames[64];
-    uint32_t instanceExtensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(NULL, &instanceExtensionCount, NULL);
-    VkExtensionProperties* instanceExtensions = (VkExtensionProperties*) malloc(sizeof(VkExtensionProperties) * instanceExtensionCount);
-    for (uint32_t i = 0; i < instanceExtensionCount; ++i) {
-        if (!strcmp(VK_KHR_SURFACE_EXTENSION_NAME, instanceExtensions[i].extensionName))
-                extensionNames[enabledExtensionCount++] = VK_KHR_SURFACE_EXTENSION_NAME;
-        if (!strcmp(PLATFORM_SURFACE_EXTENSION_NAME, instanceExtensions[i].extensionName))
-                extensionNames[enabledExtensionCount++] = PLATFORM_SURFACE_EXTENSION_NAME;
-        if (!strcmp(VK_EXT_DEBUG_REPORT_EXTENSION_NAME, instanceExtensions[i].extensionName))
-                extensionNames[enabledExtensionCount++] = VK_EXT_DEBUG_REPORT_EXTENSION_NAME;
-    }
-    free(instanceExtensions);
-
-    VkInstanceCreateInfo instInfo = {
+    uint32_t extensionCount;
+    const char** extensionNames = 0;
+    SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr);
+    extensionNames = new const char *[extensionCount];
+    SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensionNames);
+    const VkInstanceCreateInfo instInfo = {
         VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
-        NULL,                                   // pNext
-        NULL,                                   // flags
-        &appInfo,                               // pApplicationInfo
-        ARRAY_SIZE((useStdLayers ? instance_validation_layers : instance_validation_layers_alt)),           // enabledLayerCount
-        (const char* const*)(useStdLayers ? instance_validation_layers : instance_validation_layers_alt),   // ppEnabledLayerNames
-        enabledExtensionCount,                  // enabledExtensionCount
+        nullptr,                                // pNext
+        0,                                      // flags
+        nullptr,                                // pApplicationInfo
+        0,                                      // enabledLayerCount
+        nullptr,                                // ppEnabledLayerNames
+        extensionCount,                         // enabledExtensionCount
         extensionNames,                         // ppEnabledExtensionNames
     };
-    VkInstance* pVkInst;
-    vkCreateInstance(&instInfo, NULL, pVkInst);
+    VkInstance vkInst;
+    vkCreateInstance(&instInfo, nullptr, &vkInst);
 
+    uint32_t physicalDeviceCount;
+    vkEnumeratePhysicalDevices(vkInst, &physicalDeviceCount, nullptr);
+    std::vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
+    vkEnumeratePhysicalDevices(vkInst, &physicalDeviceCount, physicalDevices.data());
+    VkPhysicalDevice physicalDevice = physicalDevices[0];
+
+    uint32_t queueFamilyCount;
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, nullptr);
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, queueFamilies.data());
+
+    VkSurfaceKHR surface;
+    SDL_Vulkan_CreateSurface(window, vkInst, &surface);
+
+    uint32_t graphicsQueueIndex = UINT32_MAX;
+    uint32_t presentQueueIndex = UINT32_MAX;
+    VkBool32 support;
+    uint32_t i = 0;
+    for (VkQueueFamilyProperties queueFamily : queueFamilies) {
+        if (graphicsQueueIndex == UINT32_MAX && queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            graphicsQueueIndex = i;
+        if (presentQueueIndex == UINT32_MAX) {
+            vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, i, surface, &support);
+            if(support)
+                presentQueueIndex = i;
+        }
+        ++i;
+    }
+
+    float queuePriority = 1.0f;
+    VkDeviceQueueCreateInfo queueInfo = {
+        VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, // sType
+        nullptr,                                    // pNext
+        0,                                          // flags
+        graphicsQueueIndex,                         // graphicsQueueIndex
+        1,                                          // queueCount
+        &queuePriority,                             // pQueuePriorities
+    };
+    
+    VkPhysicalDeviceFeatures deviceFeatures = {};
+    const char* deviceExtensionNames[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+    VkDeviceCreateInfo createInfo = {
+        VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,   // sType
+        nullptr,                                // pNext
+        0,                                      // flags
+        1,                                      // queueCreateInfoCount
+        &queueInfo,                             // pQueueCreateInfos
+        0,                                      // enabledLayerCount
+        nullptr,                                // ppEnabledLayerNames
+        1,                                      // enabledExtensionCount
+        deviceExtensionNames,                   // ppEnabledExtensionNames
+        &deviceFeatures,                        // pEnabledFeatures
+    };
+    VkDevice device;
+    vkCreateDevice(physicalDevice, &createInfo, nullptr, &device);
+
+    VkQueue graphicsQueue;
+    vkGetDeviceQueue(device, graphicsQueueIndex, 0, &graphicsQueue);
+
+    VkQueue presentQueue;
+    vkGetDeviceQueue(device, presentQueueIndex, 0, &presentQueue);
+
+    SDL_Log("Initialized with errors: %s", SDL_GetError());
+    
     bool running = true;
     while(running) {
         SDL_Event windowEvent;
@@ -98,7 +115,14 @@ int main(int argc, char * argv[]) {
                 break;
             }
     }
+
+    vkDestroyDevice(device, nullptr);
+    vkDestroyInstance(vkInst, nullptr);
     SDL_DestroyWindow(window);
+    SDL_Vulkan_UnloadLibrary();
     SDL_Quit();
+
+    SDL_Log("Cleaned up with errors: %s", SDL_GetError());
+
     return 0;
 }
